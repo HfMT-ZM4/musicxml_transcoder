a1fd0294d79299c1aedb9aaa58b5edf7b3eea0c6
maccallum <john.m@ccallum.com>
2019-10-12 14:00:32 +0200

    modified the round trip makefile so that it creates and deletes the output directories

--- test/rt.mk
+++ test/rt.mk
@@ -1,14 +1,23 @@
 SCORES=./scores
 FILENAMES=test1
 OUTPUT:=$(foreach f, $(FILENAMES), $(SCORES)/jmsl4/$(f).XML)
+DIRS=$(foreach d, jmsl2 jmsl3 jmsl4 musicxml skipped, $(SCORES)/$(d))
 
-.PRECIOUS: $(foreach f, $(FILENAMES), $(SCORES)/jmsl2/$(f).XML) \
-$(foreach f, $(FILENAMES), $(SCORES)/musicxml/$(f).XML)\
-$(foreach f, $(FILENAMES), $(SCORES)/jmsl3/$(f).XML)\
-$(foreach f, $(FILENAMES), $(SCORES)/jmsl4/$(f).XML)
+.PRECIOUS: $(foreach f, $(FILENAMES), $(foreach d, $(DIRS), $(d)/$(f).XML))
 
 .PHONY: all
-all: $(OUTPUT) 
+all: $(DIRS) $(OUTPUT)
+
+$(SCORES)/jmsl2:
+	mkdir $@
+$(SCORES)/jmsl3:
+	mkdir $@
+$(SCORES)/jmsl4:
+	mkdir $@
+$(SCORES)/musicxml:
+	mkdir $@
+$(SCORES)/skipped:
+	mkdir $@
 
 $(SCORES)/jmsl4/%.XML: $(SCORES)/jmsl3/%.XML
 	java -jar jmslwriter.jar jmsl $(SCORES)/jmsl3 $(SCORES)/jmsl4 $(notdir $@)
@@ -24,13 +33,13 @@ $(SCORES)/jmsl2/%.XML: $(SCORES)/jmsl1/%.XML
 
 .PHONY: clean clean-jmsl2 clean-jmsl3 clean-jmsl4 clean-musicxml clean-skipped
 clean-jmsl4:
-	rm -rf $(SCORES)/jmsl4/*
+	rm -rf $(SCORES)/jmsl4
 clean-jmsl3:
-	rm -rf $(SCORES)/jmsl3/*
+	rm -rf $(SCORES)/jmsl3
 clean-musicxml:
-	rm -rf $(SCORES)/musicxml/*
+	rm -rf $(SCORES)/musicxml
 clean-jmsl2:
-	rm -rf $(SCORES)/jmsl2/*
+	rm -rf $(SCORES)/jmsl2
 clean-skipped:
-	rm -rf $(SCORES)/skipped/*
+	rm -rf $(SCORES)/skipped
 clean: clean-jmsl2 clean-jmsl3 clean-jmsl4 clean-musicxml clean-skipped
