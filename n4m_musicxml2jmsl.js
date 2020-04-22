f422760a7157a0e995a01d3657801a8cb62f3312
maccallum <john.m@ccallum.com>
2020-04-22 16:46:28 +0200

    fixed a bug that caused the final closing tag to be omitted from the n4m version

--- n4m_musicxml2jmsl.js
+++ n4m_musicxml2jmsl.js
@@ -10,6 +10,7 @@ Max.addHandler("filename", (filename) => {
     }
     m2j.read_musicxml(filename, function(musicxml_str){
 	m2j.musicxml2jmsl(musicxml_str, function(jmsl, skipped){
+	    jmsl += "\n";
 	    var lines = (jmsl.match(/.*\n/g)||[])
 	    //console.log("num lines = " + lines.length);
 	    Max.outlet("skipped", skipped);
